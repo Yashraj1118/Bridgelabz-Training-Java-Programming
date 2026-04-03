31b08d6eaf38ebf218b8e79c565af879435b641b
Yashraj1118 <yash.raj17182003@gmail.com>
2026-04-03 23:27:37 +0530

    [Yashraj] Add. Solution to find the perimeter of the square

---
+++ Core-Programming-Basics/CoreProgramming level 2 Problems/Perimeter.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Perimeter {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double perimeterOfSquare=sc.nextDouble();
+        double lengthOfSquare=perimeterOfSquare/4;
+        System.out.println("perimeter of square: "+ perimeterOfSquare+ " length of square: "+ lengthOfSquare);
+    }
+}
