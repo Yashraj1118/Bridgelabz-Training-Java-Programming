ecf3ffeab84d32faa84de4ff2e8e49aa44ed00af
Yashraj1118 <yash.raj17182003@gmail.com>
2026-04-03 22:57:10 +0530

    [Yashraj] Add. Solution to convert distance from km to miles

---
+++ Core-Programming-Basics/CoreProgramming level 1 Problems/ConvertKmToMiles.java
@@ -0,0 +1,8 @@
+public class ConvertKmToMiles {
+    
+    public static void main(String[] args){
+        double km=10.8;
+        double miles=km*1.6;
+        System.out.println("The distance of "+km+" in miles is: "+miles);
+    }
+}
