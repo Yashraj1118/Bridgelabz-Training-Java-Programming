7142312c30182ec1b7c55c93542883beb23ad374
Yashraj1118 <yash.raj17182003@gmail.com>
2026-04-14 16:19:50 +0530

    [Yashraj] Add. Solution to problem of book author using OOPS concepts.

---
+++ OOP-Basics/Constructors and Access Modifiers/Book.java
@@ -0,0 +1,22 @@
+public class Book {
+    String title;
+    String author;
+    double price;
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+    
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book("xyz", "abc", 1000);
+        book1.displayInfo();
+    }
+}
