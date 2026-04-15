26bca4bc037ee45359a80bc4107279a16b266bc8
Yashraj1118 <yash.raj17182003@gmail.com>
2026-04-15 11:33:35 +0530

    [Yashraj] Add. Solution to problem of the Smart home using the concepts of inheritance .

---
+++ OOP-Advanced/Inheritance/SmartHome.java
@@ -0,0 +1,37 @@
+
+class Device {
+    String deviceId;
+    String status;
+
+    public Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    public void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
+
+class Thermostat extends Device {
+    int temperatureSetting;
+
+    public Thermostat(String deviceId, String status, int temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    @Override
+    public void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
+
+public class SmartHome {
+    public static void main(String[] args) {
+        Thermostat thermostat = new Thermostat("T1001", "ON", 22);
+        thermostat.displayStatus();
+    }
+}
