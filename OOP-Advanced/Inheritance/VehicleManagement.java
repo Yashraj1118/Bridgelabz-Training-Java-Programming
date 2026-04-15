606fb5ee8a4da09d5fbabc83ae6900e596c59130
Yashraj1118 <yash.raj17182003@gmail.com>
2026-04-15 11:34:31 +0530

    [Yashraj] Add. Solution to problem of the vehicle management using the concepts of inheritance .

---
+++ OOP-Advanced/Inheritance/VehicleManagement.java
@@ -0,0 +1,74 @@
+
+
+class Vehicle {
+    String model;
+    int maxSpeed;
+
+    public Vehicle(String model, int maxSpeed) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void displayInfo() {
+        System.out.println("Model: " + model);
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+    }
+}
+
+class ElectricVehicle extends Vehicle {
+    int batteryCapacity;
+
+    public ElectricVehicle(String model, int maxSpeed, int batteryCapacity) {
+        super(model, maxSpeed);
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public void charge() {
+        System.out.println("Charging the electric vehicle...");
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Battery Capacity: " + batteryCapacity + " kWh");
+    }
+}
+interface Refuelable {
+    void refuel();
+}
+
+class PetrolVehicle extends Vehicle implements Refuelable {
+    int fuelTankCapacity;
+
+    public PetrolVehicle(String model, int maxSpeed, int fuelTankCapacity) {
+        super(model, maxSpeed);
+        this.fuelTankCapacity = fuelTankCapacity;
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println("Refueling the petrol vehicle...");
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Fuel Tank Capacity: " + fuelTankCapacity + " liters");
+    }
+}
+
+public class VehicleManagement {
+    public static void main(String[] args) {
+        ElectricVehicle ev = new ElectricVehicle("Tesla Model 3", 250, 75);
+        PetrolVehicle pv = new PetrolVehicle("Toyota Camry", 210, 50);
+
+        System.out.println("Electric Vehicle Details:");
+        ev.displayInfo();
+        ev.charge();
+
+        System.out.println("\nPetrol Vehicle Details:");
+        pv.displayInfo();
+        pv.refuel();
+    }
+    
+}
