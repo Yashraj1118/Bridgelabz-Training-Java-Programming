208f19e7f6a75160ad170a9eb61c418c9c5d2090
Yashraj1118 <yash.raj17182003@gmail.com>
2026-04-15 11:30:35 +0530

    [Yashraj] Add. Solution to problem of the Educational course using the concepts of inheritance .

---
+++ OOP-Advanced/Inheritance/EducationCourse.java
@@ -0,0 +1,58 @@
+
+
+class Course {
+    String courseName;
+    int duration;
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public void displayCourseInfo() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Duration: " + duration + " hours");
+    }
+}
+class OnlineCourse extends Course {
+    String platform;
+    boolean isRecorded;
+
+    public OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+
+    @Override
+    public void displayCourseInfo() {
+        super.displayCourseInfo();
+        System.out.println("Platform: " + platform);
+        System.out.println("Recorded: " + (isRecorded ? "Yes" : "No"));
+    }
+}
+
+class PaidOnlineCourse extends OnlineCourse {
+    double fee;
+    double discount;
+
+    public PaidOnlineCourse(String courseName, int duration, String platform, boolean isRecorded, double fee, double discount) {
+        super(courseName, duration, platform, isRecorded);
+        this.fee = fee;
+        this.discount = discount;
+    }
+
+    @Override
+    public void displayCourseInfo() {
+        super.displayCourseInfo();
+        System.out.println("Fee: $" + fee);
+        System.out.println("Discount: " + discount + "%");
+    }
+}
+
+public class EducationCourse {
+    public static void main(String[] args) {
+        PaidOnlineCourse course = new PaidOnlineCourse("Java Programming", 40, "Udemy", true, 199.99, 20);
+        course.displayCourseInfo();
+    }
+}
